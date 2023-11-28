6b5982776549f6daf0777fc7d61aa17a6e2f0a94
feoranis26 <alpdoganer26@gmail.com>
2023-11-28 15:27:43 +0300

    switched to using pose for target

--- armatron/goto_pose.py
+++ armatron/goto_pose.py
@@ -7,6 +7,7 @@ from rclpy.duration import Duration
 
 from std_msgs.msg import Bool, String
 from tf2_ros.buffer import Buffer
+import tf2_geometry_msgs
 from geometry_msgs.msg import Twist, Point, Pose, PoseStamped, Quaternion, TransformStamped
 from nav_msgs.msg import Odometry
 from tf2_ros import TransformListener
@@ -16,11 +17,10 @@ class GotoPoseNode(Node):
     def __init__(self):
         super().__init__("goto_pose")
 
-        self.source_frame = self.declare_parameter(
-          'source_frame', 'base_link').get_parameter_value().string_value
+        self.base_frame = self.declare_parameter(
+          'base_frame', 'base_link').get_parameter_value().string_value
 
-        self.target_frame = self.declare_parameter(
-          'target_frame', 'target').get_parameter_value().string_value
+        self.pose_subscriber = self.create_subscription(PoseStamped, "/goal_pose", self.pose_received)
 
         self.vel_publisher = self.create_publisher(Twist, '/cmd_vel', 1)
 
@@ -29,26 +29,35 @@ class GotoPoseNode(Node):
 
         self.timer = self.create_timer(0.1, self.update)
 
+        self.pose = None
+
     def update(self):
+        if self.pose == None:
+            return
+        
         try:
-            transform = self.tf_buffer.lookup_transform(self.source_frame, self.target_frame, rclpy.time.Time())
+            transform = self.tf_buffer.lookup_transform(self.base_frame, self.pose.header.frame_id, rclpy.time.Time())
         except TransformException as ex:
             self.get_logger().info(
-                f'Could not transform {self.source_frame} to {self.target_frame}: {ex}')
+                f'Could not transform {self.base_frame} to {self.pose.header.frame_id}: {ex}')
             return
+
+        pose_transformed = tf2_geometry_msgs.do_transform_pose(self.pose, transform)
 
         msg = Twist()
 
-        speed_x = min(max(-0.15, transform.transform.translation.x), 0.15)
-        speed_y = min(max(-0.15, transform.transform.translation.y), 0.15)
-        speed_th = min(max(-0.1, transform.transform.rotation.z), 0.1)
+        speed_x = min(max(-0.15, pose_transformed.position.x), 0.15)
+        speed_y = min(max(-0.15, pose_transformed.positionn.y), 0.15)
+        speed_th = min(max(-0.1, pose_transformed.rotation.z), 0.1)
 
         msg.linear.x = float(speed_x)
         msg.linear.y = float(speed_y)
         msg.angular.z = float(speed_th)
 
         self.vel_publisher.publish(msg)
-        
+    
+    def pose_received(self, msg):
+        self.pose = msg
 
     
 def main(args=None):
